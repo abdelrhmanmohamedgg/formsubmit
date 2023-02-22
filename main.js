3beb23367167cf84e16e6eeefd6181e685a51035
abdelrhmanmohamedgg <126091545+abdelrhmanmohamedgg@users.noreply.github.com>
2023-02-22 17:45:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('btn');
+const pass = document.getElementById('password');
+const icon = document.querySelector('.icon');
+
+btn.addEventListener('click',function(){
+    if (pass.type === 'password'  ){
+    pass.type = 'text';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
+
+
+}
+    else{
+        pass.type = 'password';
+    
+        icon.classList.add('fa-eye');
+        icon.classList.remove('fa-eye-slash');
+    
+    
+    }
+
+})
+
+
